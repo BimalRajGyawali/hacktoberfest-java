b0acbfccb188430a8fa7837fcf98c94f1192e903
Malitthh <malith.akalanka@outlook.com>
2022-10-07 16:54:18 +0530

    string reversed program

---
+++ src/main/java/org/example/Strings/ReversedString.java
@@ -0,0 +1,19 @@
+public class ReversedString {
+	
+	public static void main(String[] args) {
+		
+		String myname = "Ghost";
+		String reversedStr = "";
+		
+		for (int i = myname.length()-1;i>=0;i--) {
+			
+			reversedStr = reversedStr+myname.charAt(i);
+		}
+		System.out.println("Original "+myname);
+		System.out.println("Reversed "+reversedStr);
+	}
+	
+	
+	
+	
+}
