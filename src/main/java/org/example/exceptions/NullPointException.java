42509fb3214d5d37911e676d779f9d3ea7ffabbe
Malitthh <malith.akalanka@outlook.com>
2022-10-07 13:49:10 +0530

    null point exceptions explained with an example

---
+++ src/main/java/org/example/exceptions/NullPointException.java
@@ -0,0 +1,13 @@
+/*When a Java application tries to process an object that has a null value, a NullPointerException is raised.*/
+
+public class NullPointException {
+    	 public static void main(String[] args) {
+    	    Integer number = null;
+
+    	    if (number > 0) {
+    	      System.out.println("Positive number");
+    	    }
+    	  }
+    	}
+
+/*A basic evaluation in the example above would fail since the number (Integer) object is null, resulting in a NullPointerException.*/
