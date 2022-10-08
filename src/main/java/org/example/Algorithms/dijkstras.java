31fd5f7d2f045f6156bc31a651c46c8dfc387491
bagariaraj23 <bagariaraj5@gmail.com>
2022-10-08 14:25:04 +0530

    Dijkstras Algorithm

--- src/main/java/org/example/Algorithms/dijkstras.java
+++ src/main/java/org/example/Algorithms/dijkstras.java
@@ -4,7 +4,7 @@
 
 import java.util.*;
 
-public class DijkstraClass{
+class dijkstraClass{
     final static int MAX = 20;
     final static int infinity = 9999;
     static int n;                           //No. of vertices of G
@@ -13,31 +13,31 @@ public class DijkstraClass{
 
     public static void main(String [] args)
     {
-        ReadMatrix();
+        readMatrix();
         int source = 0;              //source vertex
         System.out.println("Enter the source vertex: ");
         source = sc.nextInt();
-        Dijkstra(s);
+        dijkstraAlgorithm(source);
     }
 
-    static void ReadMatrix()
+    static void readMatrix()
     {
         System.out.println("Enter the number of vertices: ");
         n = sc.nextInt();
         System.out.println("Enter the adjacency matrix: ");
-        for(int i=0; i<n; i++)
-            for(int j=0; j<n; j++)
+        for(int i=1; i<=n; i++)
+            for(int j=1; j<=n; j++)
                 G[i][j] = sc.nextInt();
     }
 
-    static void Dijkstra(int source)
+    static void dijkstraAlgorithm(int source)
     {
         int S[] = new int[MAX];     //Set of vertices whose shortest path from source is found
         int dist[] = new int[MAX];     //Distance of vertices from source
 
-        int u,v;
+        int u = 1,v=0;
         int i;
-        for(i = 0; i<n; i++)
+        for(i = 1; i<=n; i++)
         {
             S[i] = 0;               //Initially no vertex is included in S
             dist[i] = G[source][i]; //Distance of vertices from source
@@ -49,7 +49,7 @@ public class DijkstraClass{
         while(i<=n)
         {
             int min = infinity;
-            for(int w=0; w<n; w++)
+            for(int w=1; w<=n; w++)
             {
                 if(S[w] == 0 && dist[w] < min)
                 {
@@ -59,14 +59,14 @@ public class DijkstraClass{
             }
             S[u] = 1;
             i++;
-            for(v=0; v<n; v++)
+            for(v=1; v<=n; v++)
             {
                 if(S[v] == 0 && G[u][v] != infinity && dist[u] + G[u][v] < dist[v])
                     dist[v] = dist[u] + G[u][v];
             }
         }
-        for(i=0;i<n;i++)
-            if(i!=s)
-            System.out.println(i+":"+d[i]);
+        for(i=1;i<=n;i++)
+            if(i!=source)
+            System.out.println(i+":"+dist[i]);
     }
 }
