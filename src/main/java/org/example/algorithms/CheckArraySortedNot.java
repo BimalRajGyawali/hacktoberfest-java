d1e4dc85c9cf8785547d5ac064bf586cc48750e2
Yadnyesh-Dashpute <115160579+Yadnyesh-Dashpute@users.noreply.github.com>
2022-10-09 17:41:27 +0530

    Checking that the Given Array Is Sorted or Not using Recurssion.

---
+++ src/main/java/org/example/algorithms/CheckArraySortedNot.java
@@ -0,0 +1,32 @@
+public class  CheckArraySortedNot{
+    
+
+public static boolean arraySorted(int array[], int i)
+
+{
+if(i == array.length-1)
+{
+    return true;
+}
+
+
+    if(array[i] > array[i+1])
+    {
+        return false;
+    }
+
+    return arraySorted(array, i+1);
+
+
+
+}
+    public static void main(String[] args) {
+    
+        int array[] = {2,4,9,8};
+
+        System.out.println(arraySorted(array, 0));
+
+    }
+
+
+}
