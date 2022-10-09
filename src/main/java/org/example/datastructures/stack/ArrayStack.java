c7969aed2d10a835fab6786d220769eca95178da
Zohaib Hassan Soomro <19SW42@students.muet.edu.pk>
2022-10-09 16:59:34 +0500

    Update ArrayStack.java

--- src/main/java/org/example/datastructures/stack/ArrayStack.java
+++ src/main/java/org/example/datastructures/stack/ArrayStack.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package stack;
+package org.example.datastructures.stack;
 
 import java.util.NoSuchElementException;
 
