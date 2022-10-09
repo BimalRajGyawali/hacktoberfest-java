9a8a6aa1995d1794e5e9e218a521c32870af56ca
Yadnyesh-Dashpute <115160579+Yadnyesh-Dashpute@users.noreply.github.com>
2022-10-10 01:12:57 +0530

    Update FactorialNNumber.java

--- src/main/java/org/example/algorithms/FactorialNNumber.java
+++ src/main/java/org/example/algorithms/FactorialNNumber.java
@@ -1,7 +1,7 @@
 package org.example.algorithms;
 public class FactorialNNumber {
     
-    public static int Factorial(int n)
+    public static int factorial(int n)
     {
     
 if(n == 0)
@@ -9,11 +9,11 @@ if(n == 0)
     return 1;
 }
 
-int FactorialNM1 = Factorial(n-1); //---> Factorial(n-1);
+int factorialNM1 = factorial(n-1); //---> Factorial(n-1);
 
-int FactorialN = n * FactorialNM1;
+int factorialN = n * factorialNM1;
 
-return FactorialN;
+return factorialN;
 
 
     }
@@ -21,7 +21,7 @@ public static void main(String[] args) {
         
 int n = 5;
 
-System.out.println( Factorial(n));
+System.out.println( factorial(n));
 
  }
 }
