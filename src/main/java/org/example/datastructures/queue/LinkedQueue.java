7411902c778d414d416131ec4c2ef36d5d700501
Zohaib Hassan Soomro <19SW42@students.muet.edu.pk>
2022-10-09 16:58:58 +0500

    Update LinkedQueue.java

--- src/main/java/org/example/datastructures/queue/LinkedQueue.java
+++ src/main/java/org/example/datastructures/queue/LinkedQueue.java
@@ -3,8 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package queue;
-
+package org.example.datastructures.queue;
 /**
  *
  * @author Zohaib Hassan Soomro
