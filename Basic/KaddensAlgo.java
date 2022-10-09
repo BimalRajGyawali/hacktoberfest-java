201e40352bcce0cf4a421ab8d4921e82951e025d
amisha rani <83347105+AmishaRani@users.noreply.github.com>
2022-10-09 16:43:01 +0530

    Update KaddensAlgo.java

--- Basic/KaddensAlgo.java
+++ Basic/KaddensAlgo.java
@@ -1,6 +1,6 @@
 public class KaddensAlgo {
     public static void main(String[] args) {
-        int[] n = {-9,4,7,-4,-6,8,6};
+        int[] n = {-9,4,7,-4,6,8,6};
         int maxSum=Integer.MIN_VALUE;
         int currentSum = 0;
         for (int i = 0; i < n.length; i++) {
