97b9b602fa5bee5d1c82099f0e0d895ed785249e
Vallabh Chugh <vallabh.iaf123@gmail.com>
2022-10-09 23:37:17 +0530

    Create EncapExample.java

---
+++ OOPS/encapsulation/EncapExample.java
@@ -0,0 +1,32 @@
+// Encapsulation in java is a process of wrapping code and data together into a single unit
+// By providing only setter or getter method, you can make the class read-only or write-only.
+
+public class EncapExample {
+   private String name;
+   private String idNum;
+   private int age;
+
+   public int getAge() {
+      return age;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public String getIdNum() {
+      return idNum;
+   }
+
+   public void setAge( int newAge) {
+      age = newAge;
+   }
+
+   public void setName(String newName) {
+      name = newName;
+   }
+
+   public void setIdNum( String newId) {
+      idNum = newId;
+   }
+}
