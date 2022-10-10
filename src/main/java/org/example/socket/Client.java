0480e7113573f5764fd9d539daa3d43bd39b1c7f
SagunDevkota <sagundevyt@gmail.com>
2022-10-10 19:47:05 +0545

    Single threaded chat app using TCP

---
+++ src/main/java/org/example/socket/Client.java
@@ -0,0 +1,39 @@
+package org.example.socket;
+
+import java.net.Socket;
+import java.util.Scanner;
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
+class Client{
+	public static void main(String[] args){
+		try{
+			System.out.println("Connecting to server.");
+
+			// make connection to server listening in localhost at post 9999
+			Socket socket = new Socket("localhost",9999);
+			System.out.println("Request Accepted.\n Connection Established.");
+
+			// creating output stream to send data from client to server
+			DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+			
+			//creating input stream to recieve data from server to client
+			DataInputStream dis = new DataInputStream(socket.getInputStream());
+			String input = "";
+			while(true){
+				System.out.print("-: ");
+				String message = new Scanner(System.in).nextLine();
+
+				// sending message from client to server
+				dos.writeUTF(message);
+				dos.flush();
+
+				//recieving message from server to client
+				input = dis.readUTF();
+				System.out.println(input);
+
+			}
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+}
