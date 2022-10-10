2a0cec3eddfc071ef374fa3c37a7c5b1e5678e04
dhruvpatidar359 <dhruvpatidar35@gmail.com>
2022-10-10 17:36:53 +0530

    Interface with example added

---
+++ src/main/java/org/example/oop/Interfaces/Interfaces.java
@@ -0,0 +1,45 @@
+package org.example.oop.Interfaces;
+
+
+interface Test{
+    int a =67;
+    void engine(int dec);
+    void horn(int kick);
+}
+abstract class Test1{
+    abstract void engine(int dec);
+    abstract  int magsewa(int dev);
+}
+
+
+
+class Car1 implements Test{
+
+    public void engine(int dec){
+
+    }
+    public void horn(int kick){
+
+    }
+
+    void mareee(){
+
+    }
+
+}
+
+ class Agsdh extends Test1{
+
+     void engine(int dec){
+
+    }
+
+
+      int magsewa(int dev){
+         return 4;
+      }
+}
+
+
+public class Interfaces {
+}
