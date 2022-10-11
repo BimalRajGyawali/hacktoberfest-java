959953df2452d7815283e95472259a09a81f17e2
dhruvpatidar359 <dhruvpatidar35@gmail.com>
2022-10-11 20:30:11 +0530

    NumberRectagnle a pattern that prints rectangle with number

---
+++ src/main/java/org/example/patterns/NumberRectangle.java
@@ -0,0 +1,33 @@
+package org.example.patterns;
+
+public class NumberRectangle {
+    public static void main(String[] args) {
+        pattern31(4);
+    }
+
+    static void pattern31(int n) {
+        int originalN = n;
+        n = 2 * n;
+        for (int row = 0; row <= n; row++) {
+            for (int col = 0; col <= n; col++) {
+                int atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row, n - col));
+                System.out.print(atEveryIndex + " ");
+            }
+            System.out.println();
+        }
+
+    }}
+/*
+This will prints
+
+
+         4 4 4 4 4 4 4
+         4 3 3 3 3 3 4
+         4 3 2 2 2 3 4
+         4 3 2 1 2 3 4
+         4 3 2 2 2 3 4
+         4 3 3 3 3 3 4
+         4 4 4 4 4 4 4
+
+         pattern like this
+ */
