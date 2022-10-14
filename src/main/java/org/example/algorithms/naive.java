22f6a3517c3773d1aa464c6cee0625dbbe4cdddf
Palak Grewal <107528614+PalakGrewal@users.noreply.github.com>
2022-10-14 11:18:07 +0530

    Create naive.java

---
+++ src/main/java/org/example/algorithms/naive.java
@@ -0,0 +1,28 @@
+public class NaiveSearch {
+ 
+    static void search(String pat, String txt)
+    {
+        int l1 = pat.length();
+        int l2 = txt.length();
+        int i = 0, j = l2 - 1;
+ 
+        for (i = 0, j = l2 - 1; j < l1;) {
+ 
+            if (txt.equals(pat.substring(i, j + 1))) {
+                System.out.println("Pattern found at index "
+                                   + i);
+            }
+            i++;
+            j++;
+        }
+    }
+     
+ 
+    public static void main(String args[])
+    {
+        String pat = "AABAACAADAABAAABAA";
+        String txt = "AABA";
+     
+        search(pat, txt);
+    }
+}
