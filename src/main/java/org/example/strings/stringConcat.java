6b57e0a052ada222c602414bbf58eb2499afbfde
Aditya kumar <97943630+Adityakumar9341@users.noreply.github.com>
2022-10-17 22:11:25 +0530

    Concatenating Two Strings using concat method

---
+++ src/main/java/org/example/strings/stringConcat.java
@@ -0,0 +1,10 @@
+package org.example.strings;
+class stringConcat{  
+ public static void main(String args[]){  
+   String s1="Virat ";  
+   String s2="Kohli"; 
+   //using concat() method
+   String s3=s1.concat(s2);  
+   System.out.println(s3);//Virat Kohli will be printed
+  }  
+} 
