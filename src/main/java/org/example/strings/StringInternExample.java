cb97c4aff8731bb8603a3653ff96a6895084c9da
Yuvika-Nagar <93426407+Yuvika-Nagar@users.noreply.github.com>
2022-10-17 21:24:35 +0530

    Example of Intern in string

---
+++ src/main/java/org/example/strings/StringInternExample.java
@@ -0,0 +1,9 @@
+package org.example.strings;
+public class StringInternExample{  
+public static void main(String args[]){  
+String s1=new String("hello");  
+String s2="hello";  
+String s3=s1.intern();//returns string from pool, now it will be same as s2  
+System.out.println(s1==s2);//false because reference variables are pointing to different instance  
+System.out.println(s2==s3);//true because reference variables are pointing to same instance  
+}}  
