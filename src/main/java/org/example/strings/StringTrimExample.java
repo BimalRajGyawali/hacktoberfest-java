3e206402f8e4add3b5bfd5906e9f3500a7daf91a
Yuvika-Nagar <93426407+Yuvika-Nagar@users.noreply.github.com>
2022-10-17 21:21:10 +0530

    Example of String trim method in java

---
+++ src/main/java/org/example/strings/StringTrimExample.java
@@ -0,0 +1,7 @@
+package org.example.strings;
+public class StringTrimExample{  
+public static void main(String args[]){  
+String s1="  hello string   ";  
+System.out.println(s1+"Hacktober Fest");//without trim()  
+System.out.println(s1.trim()+"Hacktober Fest");//with trim()  
+}}  
