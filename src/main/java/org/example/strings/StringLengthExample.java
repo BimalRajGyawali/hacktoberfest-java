2cbdd74ba5523e056b2811ba579f360a25c4b4a7
Amisha1210G <116013375+Amisha1210G@users.noreply.github.com>
2022-10-17 21:36:19 +0530

    Example of String length

---
+++ src/main/java/org/example/strings/StringLengthExample.java
@@ -0,0 +1,7 @@
+package org.example.strings;
+public class StringLengthExample{  
+public static void main(String args[]){  
+String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+System.out.println(txt.length());
+  // the length of the string is 26
+}}  
