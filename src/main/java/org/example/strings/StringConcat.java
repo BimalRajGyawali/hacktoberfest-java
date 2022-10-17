09e4a825217661471ac51f7b52ec394dd8a57075
Lalit Pokhrel <lalit.pokhrel@selise.ch>
2022-10-18 01:18:23 +0600

    String concat program

---
+++ src/main/java/org/example/strings/StringConcat.java
@@ -0,0 +1,19 @@
+package org.example.strings;
+
+/**
+ * @author lit-poks
+ * @Date 10/18/22
+ */
+public class StringConcat {
+    public static void main(String[] args) {
+
+        String var = "String Concatenation with"
+        String hactoberFestString = "Hacktober fest String"
+
+        // concat string
+        String result = var.concat(hactoberFestString)
+
+        //print string
+        System.out.println(result);
+    }
+}
