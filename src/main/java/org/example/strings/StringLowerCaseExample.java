d65b20d06cddc3c10a9dcbe4d01a08ed14f6b61c
Amisha1210G <116013375+Amisha1210G@users.noreply.github.com>
2022-10-17 21:46:11 +0530

    String Lower case example

---
+++ src/main/java/org/example/strings/StringLowerCaseExample.java
@@ -0,0 +1,7 @@
+package org.example.strings;
+public class StringLowerCaseExample{  
+public static void main(String args[]){  
+String s1="JAVATPOINT HELLO stRIng";  
+String s1lower=s1.toLowerCase();  
+System.out.println(s1lower);  
+}}  
