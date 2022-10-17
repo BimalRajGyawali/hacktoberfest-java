ed464c85e1d32ce0ebe37448a09a228c135089af
Amisha1210G <116013375+Amisha1210G@users.noreply.github.com>
2022-10-17 21:41:41 +0530

    String Upper Case Example

---
+++ src/main/java/org/example/strings/StringUpperCaseExample.java
@@ -0,0 +1,7 @@
+package org.example.strings;
+public class StringUpperExample{  
+public static void main(String args[]){  
+String s1="hello string";  
+String s1upper=s1.toUpperCase();  
+System.out.println(s1upper);  
+}}  
