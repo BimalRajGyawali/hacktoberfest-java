25341d16ae916eedea19fdf80c835cfbb31405c8
Aditya kumar <97943630+Adityakumar9341@users.noreply.github.com>
2022-10-18 12:38:56 +0530

    Replacing Character in a String

---
+++ src/main/java/org/example/strings/CharReplaceInString.java
@@ -0,0 +1,8 @@
+package org.example.strings;
+class CharReplaceInString{  
+public static void main(String args[]){  
+String s1="Github is a very good Platform";  
+String replaceString=s1.replace('a','e');//replaces all occurrences of 'a' to 'e'  
+System.out.println(replaceString);  
+}
+}  
