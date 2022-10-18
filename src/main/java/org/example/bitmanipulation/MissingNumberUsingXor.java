9e1ed07b9a22241d1c64835407ba1e879273d609
Rishi1210K <116090951+Rishi1210K@users.noreply.github.com>
2022-10-18 20:12:51 +0530

    Finding Missing Number in an Array using Xor

---
+++ src/main/java/org/example/bitmanipulation/MissingNumberUsingXor.java
@@ -0,0 +1,19 @@
+package org.example.bitmanipulation;
+class MissingNumberUsingXor{
+  public static void main(String args[]) {
+    int[] arr = new int[] {1,2,3,5,6}
+    int xor1 =0;
+    int xor2 =0;
+    for(int i=1;i<=n;i++)
+    {
+      xor1 ^= i;
+    }
+    for(int i=0;i<arr.length;i++)
+    {
+      xor2 ^= arr[i];
+    }
+    int ans = xor1^xor2;
+    System.out.println(ans);//will print 4
+  }
+}
+  
