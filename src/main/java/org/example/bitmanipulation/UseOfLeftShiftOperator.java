b43646055da5c729817ee27c26bc60baeb88f5e2
Rishi1210K <116090951+Rishi1210K@users.noreply.github.com>
2022-10-18 20:28:07 +0530

    Create UseOfLeftShiftOperator.java

---
+++ src/main/java/org/example/bitmanipulation/UseOfLeftShiftOperator.java
@@ -0,0 +1,8 @@
+package org.example.bitmanipulation;
+public class UseOfLeftShiftOperator {
+  public static void main(String[] args) {
+    int operand1 = 5 
+    int output = operand1 << 1;
+    System.out.println(output);//will print 10
+  }
+}
