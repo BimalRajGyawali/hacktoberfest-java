c258a29ccf3d57a753c1a177904bfdd053902db1
Shruti-bansal30 <116055958+Shruti-bansal30@users.noreply.github.com>
2022-10-18 11:03:41 +0530

    Substring of a String

---
+++ src/main/java/org/example/strings/SubstringExample.java
@@ -0,0 +1,12 @@
+package org.example.strings;
+import java.util.*;
+public class SubstringExample {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start,end));
+    }
+}
