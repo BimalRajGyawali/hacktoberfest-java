ae02527f51631f7049baf62fcd4fd503862be73d
Amisha1210G <116013375+Amisha1210G@users.noreply.github.com>
2022-10-18 10:10:59 +0530

    update String Lower Case Example

---
+++ src/main/java/org/example/strings/StringLowerCaseExample.java
@@ -0,0 +1,14 @@
+package org.example.strings;
+
+public class StringLowerCaseExample{  
+
+  public static void main(String args[]){  
+
+    String s1="HELLO stRIng";  
+
+    String s1lower=s1.toLowerCase();  
+
+    System.out.println(s1lower);  
+
+  }
+}  
