83dd7c9b816a4b6443490e8e635b8c42590cd071
Aditya kumar <97943630+Adityakumar9341@users.noreply.github.com>
2022-10-18 12:49:21 +0530

    Comparing Two Strings ignoring case

---
+++ src/main/java/org/example/strings/StringEqualsIgnoreCase.java
@@ -0,0 +1,12 @@
+package org.example.strings;
+class StringEqualsIgnoreCase{  
+public static void main(String args[]){  
+String s1="Aditya";  
+String s2="Aditya";  
+String s3="AdiTya";  
+String s4="java";  
+System.out.println(s1.equalsIgnoreCase(s2));//true because content and case both are same  
+System.out.println(s1.equalsIgnoreCase(s3));//true because case is ignored  
+System.out.println(s1.equalsIgnoreCase(s4));//false because content is not same  
+}
+}  
