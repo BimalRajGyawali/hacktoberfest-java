08976331b4aa934693d33d48a5d008d71fa475f2
garvit999 <97027901+garvit999@users.noreply.github.com>
2022-10-18 22:26:42 +0530

    Best Time to Buy and Sell Stock

---
+++ src/main/java/org/example/leetcode/BuyAndSellStock.java
@@ -0,0 +1,25 @@
+package org.example.leetcode;
+public class BuyAndSellStock {
+
+    public static int maxProfit(int[] prices) {
+       int min = Integer.MAX_VALUE;
+       int profit = 0;
+       int currprofit  = 0;
+       
+       for(int i = 0; i < prices.length; i++){
+           if(prices[i] < min){
+               min = prices[i];
+           }
+           currprofit = prices[i] - min;
+           if(profit < currprofit){
+               profit = currprofit;
+           }
+       }
+       return profit;
+   }
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+    
+}
