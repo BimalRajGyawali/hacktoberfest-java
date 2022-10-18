31d684adf5354bcc49edb12da8eb7d716b3c61a8
VickyKRyadav7 <116100824+VickyKRyadav7@users.noreply.github.com>
2022-10-18 22:24:34 +0530

    Checking Kth Bit is a SetBit or not

---
+++ src/main/java/org/example/bitmanipulation/CheckKthBit.java
@@ -0,0 +1,18 @@
+package org.example.bitmanipulation;
+class CheckKthBit {
+    public static void main(String args[])
+    {
+      int n = 4;
+      int k = 2;
+      n = n>>k;
+      int newNum = n&1;
+      if(newNum==0)
+      {
+        System.out.println(k +" bit is not a Setbit");
+      }
+      else{
+        System.out.println(k + " bit is a Setbit");//will execute
+      }
+    }
+}
+        
