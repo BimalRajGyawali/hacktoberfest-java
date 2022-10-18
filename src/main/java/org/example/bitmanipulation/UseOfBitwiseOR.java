d6c5b2a374c960295d95f0677a88ed633db62957
Shruti-bansal30 <116055958+Shruti-bansal30@users.noreply.github.com>
2022-10-18 19:25:53 +0530

    Use Of Bitwise OR in bit manipulation

---
+++ src/main/java/org/example/bitmanipulation/UseOfBitwiseOR.java
@@ -0,0 +1,13 @@
+package org.example.bitmanipulation;
+
+public class UseOfBitwiseOR {
+
+  public static void main(String[] args) {
+
+    int operand1 = 15, operand2 = 27, output = operand1 | operand2;
+
+    System.out.println(output);
+
+  }
+  
+}
