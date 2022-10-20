e9cfbe7fe2c10dce8b7a5e77dfbb88ee015eebdb
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2022-10-20 23:15:24 +0530

    Printing Permutation Using RECURSION

---
+++ src/main/java/org/example/algorithms/recursion/Print_ALL_Permutation.java
@@ -0,0 +1,28 @@
+// Printing All Poisible Permutation of Given String Using RECURSION
+
+import java.util.Scanner;
+
+public class Print_ALL_Permutation {
+
+    private static void print(String s,String ans,int len,String x){
+        if(len == ans.length()){
+            System.out.println(ans);
+            return;
+        }
+        for(int i=0;i<s.length();i++){
+            char ch= s.charAt(i);
+            if(ans.length()==0  || x.contains(ch+"")== false)
+                print(s.substring(0,i)+s.substring(i+1),ans+ch,len,x);
+            else
+                return;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String str= sc.next();
+        print(str,"",str.length()," ");
+    }
+}
+
+
