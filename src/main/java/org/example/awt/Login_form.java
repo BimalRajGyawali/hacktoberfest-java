c2d9627f5bd57afdc26f7bb109d49e9e3283feac
Kunalraj Singh <73094875+kunalraj0@users.noreply.github.com>
2022-10-23 23:06:32 +0530

    Create Login_form.java

---
+++ src/main/java/org/example/awt/Login_form.java
@@ -0,0 +1,106 @@
+import javax.swing.*;  
+import java.awt.*;  
+import java.awt.event.*;  
+import java.lang.Exception;  
+class abc
+{  
+   
+    public static void main(String arg[])  
+    {  
+        try  
+        {  
+           
+            CreateLoginForm form = new CreateLoginForm();  
+            form.setSize(300,100);  
+            form.setVisible(true);  
+        }  
+        catch(Exception e)  
+        {    
+           
+            JOptionPane.showMessageDialog(null, e.getMessage());  
+        }  
+    }  
+}  
+
+ class NewPage extends JFrame  
+{  
+    //constructor  
+    NewPage()  
+    {  
+        setDefaultCloseOperation(javax.swing.  
+        WindowConstants.DISPOSE_ON_CLOSE);  
+        setTitle("Welcome");  
+        setSize(400, 200);  
+    }  
+}  
+ 
+class CreateLoginForm extends JFrame implements ActionListener  
+{  
+   
+    JButton b1;  
+    JPanel newPanel;  
+    JLabel userLabel, passLabel;  
+    final JTextField  textField1, textField2;  
+     
+
+    CreateLoginForm()  
+    {    
+         
+       
+        userLabel = new JLabel();  
+        userLabel.setText("Username");      
+         
+       
+        textField1 = new JTextField(15);    
+ 
+       
+        passLabel = new JLabel();  
+        passLabel.setText("Password");      
+         
+       
+        textField2 = new JPasswordField(15);    
+         
+       
+        b1 = new JButton("SUBMIT");
+         
+       
+        newPanel = new JPanel(new GridLayout(3, 1));  
+        newPanel.add(userLabel);    
+        newPanel.add(textField1);  
+        newPanel.add(passLabel);    
+        newPanel.add(textField2);  
+        newPanel.add(b1);          
+         
+       
+        add(newPanel, BorderLayout.CENTER);  
+         
+       
+        b1.addActionListener(this);    
+        setTitle("LOGIN FORM");        
+    }  
+     
+   
+    public void actionPerformed(ActionEvent ae)    
+    {  
+        String userValue = textField1.getText();        
+        String passValue = textField2.getText();        
+         
+       
+        if (userValue.equals("admin") && passValue.equals("pass")) {  
+             
+           
+            NewPage page = new NewPage();  
+             
+           
+            page.setVisible(true);  
+             
+           
+            JLabel wel_label = new JLabel("Welcome: "+userValue);  
+            page.getContentPane().add(wel_label);  
+        }  
+        else{  
+           
+            System.out.println("Please enter valid username and password");  
+        }  
+    }  
+}  
