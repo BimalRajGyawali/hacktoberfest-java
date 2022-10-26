0648c2dcb6a7c673352153c4442a9d5641a82193
shreyash2610 <57799693+shreyash2610@users.noreply.github.com>
2022-10-26 19:59:40 +0530

    Create ValidParantheses.java

---
+++ src/main/java/org/example/leetcode/ValidParantheses.java
@@ -0,0 +1,26 @@
+package org.example.leetcode;
+public class isValid {
+    public boolean isValid(String s) {
+ Stack<Character> leftSymbols = new Stack<>();
+        // Loop for each character of the string
+        for (char c : s.toCharArray()) {
+            // If left symbol is encountered
+            if (c == '(' || c == '{' || c == '[') {
+                leftSymbols.push(c);
+            }
+            // If right symbol is encountered
+            else if (c == ')' && !leftSymbols.isEmpty() && leftSymbols.peek() == '(') {
+                leftSymbols.pop();
+            } else if (c == '}' && !leftSymbols.isEmpty() && leftSymbols.peek() == '{') {
+                leftSymbols.pop();
+            } else if (c == ']' && !leftSymbols.isEmpty() && leftSymbols.peek() == '[') {
+                leftSymbols.pop();
+            }
+            // If none of the valid symbols is encountered
+            else {
+                return false;
+            }
+        }
+        return leftSymbols.isEmpty();
+    }
+}
