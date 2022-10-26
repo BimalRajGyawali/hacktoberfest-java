975f48cac9dfabec29a0521e2c8a96534f769417
syyam <syyamnoor@gmail.com>
2022-10-26 16:35:59 +0500

    add leet code solutions

---
+++ src/main/java/org/example/leetcode/MaximumSubArray.java
@@ -0,0 +1,20 @@
+class MaximumSubArray
+{
+    public int maxSubArray(int[] nums)
+    {
+        int currSum = 0;
+        int maxArrSum = nums[0];
+        for (int num : nums)
+        {
+            if (currSum < 0)
+            {
+                // ignoring the negatrive integers because they do not contribute anything
+                currSum = 0;
+            }
+            currSum += num;
+            // finding the maximum between max we calculated and the max we will get upon adding the next number in the array
+            maxArrSum = Math.max(maxArrSum, currSum);
+        }
+        return maxArrSum;
+    }
+}
