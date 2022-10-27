6ce1f1eab687e4dc275bcaa8e19433ba8be085c6
Swapnil Nangare <56465439+7smn2219@users.noreply.github.com>
2022-10-27 18:08:55 +0530

    Added WordLadder

---
+++ src/main/java/org/example/leetcode/WordLadder.java
@@ -0,0 +1,109 @@
+package org.example.leetcode;
+
+/**
+ * WordLadder
+ * https://leetcode.com/problems/word-ladder/
+ */
+
+
+public class WordLadder {
+    private int L;
+    private Map<String, List<String>> allComboDict;
+
+    WordLadder() {
+        this.L = 0;
+
+        // Dictionary to hold combination of words that can be formed,
+        // from any given word. By changing one letter at a time.
+        this.allComboDict = new HashMap<>();
+    }
+
+    private int visitWordNode(
+            Queue<Pair<String, Integer>> Q,
+            Map<String, Integer> visited,
+            Map<String, Integer> othersVisited) {
+
+        for (int j = Q.size(); j > 0; j--) {
+            
+            Pair<String, Integer> node = Q.remove();
+            String word = node.getKey();
+            int level = node.getValue();
+
+            for (int i = 0; i < this.L; i++) {
+
+                // Intermediate words for current word
+                String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
+
+                // Next states are all the words which share the same intermediate state.
+                for (String adjacentWord : this.allComboDict.getOrDefault(newWord, new ArrayList<>())) {
+                    // If at any point if we find what we are looking for
+                    // i.e. the end word - we can return with the answer.
+                    if (othersVisited.containsKey(adjacentWord)) {
+                        return level + othersVisited.get(adjacentWord);
+                    }
+
+                    if (!visited.containsKey(adjacentWord)) {
+
+                        // Save the level as the value of the dictionary, to save number of hops.
+                        visited.put(adjacentWord, level + 1);
+                        Q.add(new Pair(adjacentWord, level + 1));
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+        if (!wordList.contains(endWord)) {
+            return 0;
+        }
+
+        // Since all words are of same length.
+        this.L = beginWord.length();
+
+        wordList.forEach( word -> {
+            for (int i = 0; i < L; i++) {
+                // Key is the generic word
+                // Value is a list of words which have the same intermediate generic word.
+                String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
+                List<String> transformations =
+                        this.allComboDict.getOrDefault(newWord, new ArrayList<>());
+                transformations.add(word);
+                this.allComboDict.put(newWord, transformations);
+            }
+        });
+
+        // Queues for birdirectional BFS
+        // BFS starting from beginWord
+        Queue<Pair<String, Integer>> Q_begin = new LinkedList<>();
+        // BFS starting from endWord
+        Queue<Pair<String, Integer>> Q_end = new LinkedList<>();
+        Q_begin.add(new Pair(beginWord, 1));
+        Q_end.add(new Pair(endWord, 1));
+
+        // Visited to make sure we don't repeat processing same word.
+        Map<String, Integer> visitedBegin = new HashMap<>();
+        Map<String, Integer> visitedEnd = new HashMap<>();
+        visitedBegin.put(beginWord, 1);
+        visitedEnd.put(endWord, 1);
+        int ans = -1;
+        
+        while (!Q_begin.isEmpty() && !Q_end.isEmpty()) {
+            
+            // Progress forward one step from the shorter queue
+            if (Q_begin.size() <= Q_end.size()) {
+                ans = visitWordNode(Q_begin, visitedBegin, visitedEnd);
+            } else {
+                ans = visitWordNode(Q_end, visitedEnd, visitedBegin);    
+            }
+
+            if (ans > -1) {
+                return ans;
+            }
+        }
+
+        return 0;
+    }
+}
