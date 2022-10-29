e60d6b9c689090a96eafbb7cabc248f92037575c
nasareen <nasareen.hy@gmail.com>
2022-10-29 19:09:04 +0530

    Leetcode move zeroes

---
+++ src/main/java/org/example/leetcode/MoveZeroes.java
@@ -0,0 +1,18 @@
+package org.example.leetcode;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index++] = nums[i];
+            }
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
