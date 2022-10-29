4587663179b9307ac04f984a387c2a816bf6a0bd
Kanha gupta <kanhag4163@gmail.com>
2022-10-29 14:19:33 +0530

    Question No. 22

---
+++ src/main/java/org/example/leetcode/GenerateParenthesis.java
@@ -0,0 +1,28 @@
+package org.example.leetcode;
+//Question number 22
+
+class GenerateParenthesis {
+    public List<String> generateParenthesis(int n) {
+        List<String> combs = new ArrayList<>();
+        generate(n, n, new StringBuilder(), combs);
+        return combs;
+    }
+
+    public void generate(int open, int close, StringBuilder sb, List<String> combs){
+        if(open == 0 && close == 0){
+            combs.add(sb.toString());
+            return;
+        }
+        if(open > 0){
+            sb.append("(");
+            generate(open - 1, close, sb, combs);
+            sb.delete(sb.length() - 1, sb.length());
+        }
+
+        if(close > 0 && open < close){
+            sb.append(")");
+            generate(open, close - 1, sb, combs);
+            sb.delete(sb.length() - 1, sb.length());
+        }
+    }
+}
