d6d2b5bc2b5153257405e6e82bb847e94134897b
lelefolkl <lele.folkl@hotmail.com>
2022-10-31 01:49:08 -0300

    added a new java method to calculate farenheit with celcius

---
+++ src/main/java/org/example/algorithms/FarenheitToCelcius.java
@@ -0,0 +1,11 @@
+public class FarenheitToCelcius {
+    static float result =0;
+    static float conv(float n){
+        farenheit = (n*(5/9) - 32);
+        return farenheit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println((10));
+    }
+}
