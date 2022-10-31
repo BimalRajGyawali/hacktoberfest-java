ae3749a0638bdeede402e532022ff4142bf9e556
Kanha gupta <kanhag4163@gmail.com>
2022-10-31 20:05:33 +0530

    Question No. 15

---
+++ src/main/java/org/example/leetcode/Threesum.java
@@ -0,0 +1,32 @@
+package org.example.leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+public class Threesum {
+    public static void main(String[] args) {
+        int[] arr={-1,0,1,2,4}
+        System.out.println(threesum(arr));
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> resultSet = new HashSet();
+
+        Set<Integer> duplicatedSet = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length - 2; i++) {
+            if (!duplicatedSet.add(nums[i])) continue;
+
+            for (int j = i + 1; j < nums.length; j++) {
+                int value = 0 - nums[i] - nums[j];
+                if (map.containsKey(value) && map.get(value) == i) {
+                    List<Integer> list = new ArrayList<>(Arrays.asList(nums[i], nums[j], value));
+                    Collections.sort(list);
+                    resultSet.add(list);
+                }
+                map.put(nums[j], i);
+            }
+        }
+        return new ArrayList<>(resultSet) ;
+    }
+}
+
